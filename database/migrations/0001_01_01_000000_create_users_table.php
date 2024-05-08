4bc0b5a76b04d765b823139da53b8b71e4dcb401
LuisFel123 <felipe_ramirez1234@hotmail.com>
2024-05-07 20:39:40 -0600

    fecht: Se agrego la api registrar usuarios, y verificar si existen

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('two_factor_secret')->nullable(); // Sin la cláusula after
+            $table->string('two_factor_recovery_codes')->nullable(); // Sin la cláusula after
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
